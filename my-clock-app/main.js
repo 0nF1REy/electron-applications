6806513d33711e070a7a0c607a4aa7e059f8726a
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-02 01:08:06 -0300

    main: feat (my-clock-app) - Updated "package.json" description, enhance main.js structure, and add preload.js for DOM content loading

--- my-clock-app/main.js
+++ my-clock-app/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 let mainWindow;
 
@@ -7,20 +8,28 @@ function createWindow() {
     width: 800,
     height: 400,
     resizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    autoHideMenuBar: true,
     frame: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
   });
 
   mainWindow.loadFile("index.html");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
 });
