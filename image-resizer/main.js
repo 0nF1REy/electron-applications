32b08c2fb614d1d0bcde977d4273f20071a9112b
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-03 02:11:48 -0300

    main: fix (menu) - Restored the 'File' menu with 'Quit' option for improved application navigation

--- image-resizer/main.js
+++ image-resizer/main.js
@@ -85,16 +85,16 @@ const menu = [
         },
       ]
     : []),
-  // {
-  //   label: 'File',
-  //   submenu: [
-  //     {
-  //       label: 'Quit',
-  //       click: () => app.quit(),
-  //       accelerator: 'CmdOrCtrl+W',
-  //     },
-  //   ],
-  // },
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit(),
+        accelerator: "CmdOrCtrl+W",
+      },
+    ],
+  },
   ...(isDev
     ? [
         {
