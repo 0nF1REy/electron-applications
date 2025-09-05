badd679ce629c631249c561692345c3848338f3f
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-05 02:38:38 -0300

    main: chore (my-pomodoro-app) - Updated package version and metadata; add type declarations for assets; modify tsconfig for improved type handling

---
+++ my-pomodoro-app/src/types/index.d.ts
@@ -0,0 +1,50 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.ico" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.mp3" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.otf" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.icns" {
+  const value: string;
+  export default value;
+}
+
+declare global {
+  interface Window {
+    electronAPI?: any;
+  }
+}
