df6a338157f6c9beafa9339f1a1012baeb782485
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-07 01:27:32 -0300

    main: feat (my-electron-app) - Implemented IPC communication to expose ping functionality in the renderer process

--- my-electron-app/renderer.js
+++ my-electron-app/renderer.js
@@ -1,2 +1,10 @@
+const func = async () => {
+  const response = await window.versions.ping();
+  console.log(response);
+};
+
 const information = document.getElementById("info");
+
+func();
+
 information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`;
