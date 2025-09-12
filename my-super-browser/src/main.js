b008ae70cc2722e96d80f17d3fa3119046232039
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-12 00:42:02 -0300

    main: feat (index.html, main.js, renderer.js) - Added webview element and enabled webviewTag in main process for improved browsing capabilities

--- my-super-browser/src/main.js
+++ my-super-browser/src/main.js
@@ -1,6 +1,6 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'node:path';
-import started from 'electron-squirrel-startup';
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import started from "electron-squirrel-startup";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -14,17 +14,20 @@ const createWindow = () => {
     height: 600,
     autoHideMenuBar: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
+      webviewTag: true,
     },
   });
 
-  mainWindow.setAlwaysOnTop(true, 'screen');
+  mainWindow.setAlwaysOnTop(true, "screen");
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
   // Open the DevTools.
@@ -39,7 +42,7 @@ app.whenReady().then(() => {
 
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -49,8 +52,8 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
--- my-super-browser/src/renderer.js
+++ my-super-browser/src/renderer.js
@@ -1,33 +1,30 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+import "./index.css";
 
-import './index.css';
+// Buttons
+const backButton = document.getElementById("back-button");
 
-console.log(
-  '👋 This message is being logged by "renderer.js", included via Vite',
-);
+const forwardButton = document.getElementById("forward-button");
+
+const reloadButton = document.getElementById("reload-button");
+
+const newWindowButton = document.getElementById("new-window-button");
+
+const goButton = document.getElementById("go");
+
+// Url Field
+const urlInputField = document.getElementById("url-input");
+
+// Webview
+const webview = document.getElementById("webview");
+
+urlInputField.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    handleUrl();
+  }
+});
+
+function handleUrl() {
+  const inputUrl = urlInputField.value;
+  webview.src = inputUrl;
+}
