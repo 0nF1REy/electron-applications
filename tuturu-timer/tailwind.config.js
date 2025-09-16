4dbb20335660cda3f5124b33c034b96fab447247
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-16 02:36:04 -0300

    main: feat (tuturu-timer) - Added background images and extended Tailwind CSS configuration with custom colors, fonts, and text shadow utilities

--- tuturu-timer/tailwind.config.js
+++ tuturu-timer/tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin'
+
 export default {
   content: ['./src/renderer/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        digital: ['VT323', 'monospace']
+      },
+      colors: {
+        'fallout-green': '#28f700',
+        'fallout-green-dark': '#072600',
+        amber: '#FFBF00',
+        'amber-dark': '#382800',
+        'industrial-dark': '#212121',
+        'industrial-medium': '#323232'
+      },
+      backgroundImage: {
+        'background-normal': "url('./src/assets/images/background-normal.jpg')",
+        'background-edit': "url('./src/assets/images/background-edit.jpg')"
+      },
+      textShadow: {
+        glow: '0 0 5px currentColor, 0 0 10px currentColor'
+      }
+    }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value
+          })
+        },
+        { values: theme('textShadow') }
+      )
+    })
+  ]
 }
