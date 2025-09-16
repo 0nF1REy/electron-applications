ebf55dea5173eb37e2302b037912fcef1d7bea82
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-16 02:39:02 -0300

    main: refactor (tuturu-timer) - Improved component structure and styling; updated Timer props for better state management

--- tuturu-timer/src/renderer/src/App.jsx
+++ tuturu-timer/src/renderer/src/App.jsx
@@ -1,37 +1,38 @@
-import TopBarComponent from './components/TopBar'
 import { useState, useEffect } from 'react'
+import TopBarComponent from './components/TopBar'
 import Timer from './components/Timer'
 
 function App() {
   const [isOverlay, setIsOverlay] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
 
   useEffect(() => {
     const toggleOverlay = () => {
       setIsOverlay((prev) => !prev)
     }
-
     window.electron.ipcRenderer.on('overlay-mode', toggleOverlay)
-
     return () => {
       window.electron.ipcRenderer.removeAllListeners('overlay-mode')
     }
   }, [])
 
   return (
-    <>
-      <div className={!isOverlay ? 'visible' : 'invisible'}>
+    <div
+      className={`
+        h-screen w-screen overflow-hidden rounded-xl border-2 border-industrial-medium
+        transition-all duration-500 relative scanline
+        ${isEditing ? 'bg-background-edit' : 'bg-background-normal'}
+        bg-cover bg-center
+      `}
+    >
+      <div className={!isOverlay ? 'visible' : 'hidden'}>
         <TopBarComponent />
       </div>
-      <div
-        className={
-          !isOverlay
-            ? 'bg-black bg-opacity-40 p-2 rounded-b-xl'
-            : 'bg-black bg-opacity-40 p-2 rounded-xl'
-        }
-      >
-        <Timer isOverlay={isOverlay} />
+
+      <div className="p-2 h-full flex flex-col justify-center items-center">
+        <Timer isOverlay={isOverlay} isEditing={isEditing} setIsEditing={setIsEditing} />
       </div>
-    </>
+    </div>
   )
 }
 
