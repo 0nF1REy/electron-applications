35941e3483fdde315a033be2e3db7a9f966598ec
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-09-17 02:18:27 -0300

    main: feat (ipc) - Implemented URL opening functionality with new window management and updated preload script for IPC communication

--- ipc/src/renderer.js
+++ ipc/src/renderer.js
@@ -1,33 +1,17 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+const chrome = document.getElementById("chrome");
+const node = document.getElementById("node");
+const electron = document.getElementById("electron");
+const appPath = document.getElementById("app-path");
+const inputArea = document.getElementById("input-area");
+const submitButton = document.getElementById("url-submit");
 
-import './index.css';
+chrome.innerHTML += " " + versions.chrome();
+node.innerHTML += " " + versions.node();
+electron.innerHTML += " " + versions.electron();
 
-console.log(
-  '👋 This message is being logged by "renderer.js", included via Vite',
-);
+submitButton.addEventListener("click", () => {
+  const url = inputArea.value;
+  if (url.trim() !== "") {
+    openNewWindow.openUrl(url);
+  }
+});
